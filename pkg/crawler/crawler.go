bb3bae754a781562e4599522ca4d3d3b6bc8a536
anay roy <anayroy42003@gmail.com>
2020-10-04 11:56:33 +0530

    task done

--- pkg/crawler/crawler.go
+++ pkg/crawler/crawler.go
@@ -1,9 +1,7 @@
 package crawler
 
 import (
-	"bytes"
-	"crypto/sha256"
-	"errors"
+	"crypto/sha1"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -11,227 +9,103 @@ import (
 	"net/url"
 	"os"
 	"path"
-	"regexp"
-	"runtime"
-	"strings"
-	"time"
 
-	"github.com/PuerkitoBio/goquery"
-	wkhtmltopdf "github.com/SebastiaanKlippert/go-wkhtmltopdf"
 	"jaytaylor.com/html2text"
 )
 
-// Site ...
 type Site struct {
-	HtmlBody []byte
-	TextBody []byte
+	HtmlBody []byte //warning: exported type Site should have comment or be unexported (golint)
+	TextBody []byte 
 	Url      string
 	Title    string
 }
 
-// Crawler ...
 type Crawler struct {
-	Urls    []string
+	Urls    []string //warning: exported type Crawler should have comment or be unexported (golint)
 	Archive string
 	Sites   []Site
 }
 
-// Error Messages
-var (
-	errNoTitleInHtml = errors.New("No title tag in HTML response")
-	// regular expression from: https://mathiasbynens.be/demo/url-regex,
-	// by @imme_emosol
-	urlRegExp, _ = regexp.Compile(`^(https|http)://(-\.)?([^\s/?\.#-]+\.?)+(/[^\s]*)?$`)
-)
-
-// NewCrawler ...
 func NewCrawler(archive string) (Crawler, error) {
-	return Crawler{
+	return Crawler{  //warning: exported function NewCrawler should have comment or be unexported (golint)
 		Archive: archive,
 	}, nil
 }
 
-// Save ...
 func (c *Crawler) Save() error {
-	ensureArchive(c.Archive)
-
-	// save all sites one by one
+	ensureArchive(c.Archive)   //warning: exported method Crawler.Save should have comment or be unexported (golint)
 	for _, s := range c.Sites {
 		fmt.Printf("Saving %s...\n", s.Url)
-
 		parsed, err := url.Parse(s.Url)
 		if err != nil {
 			return err
 		}
-
 		d := parsed.Host
 
-		// get current time
-		t := time.Now()
-		var timestamp string
+		// generate a file title
+		h := sha1.New()
+		io.WriteString(h, s.Url)
+		s.Title = fmt.Sprintf("%x", h.Sum(nil))
 
-		if runtime.GOOS == "windows" {
-			timestamp = "%d-%02d-%02dT%02d_%02d_%02d" // use underscores instead of colons
-		} else {
-			timestamp = "%d-%02d-%02dT%02d:%02d:%02d"
-		}
-
-		dateTime := fmt.Sprintf(timestamp,
-			t.Year(), t.Month(), t.Day(),
-			t.Hour(), t.Minute(), t.Second())
-
-		// create the sub folder inside domain folder date-time as name
-		domainSubPath := path.Join(c.Archive, d, dateTime)
-		err = os.MkdirAll(domainSubPath, 0700)
+		// create the domain folder if needed
+		err = os.MkdirAll(path.Join(c.Archive, d), 0777)
 		if err != nil {
 			return err
 		}
 
 		// save the html
 		htmlFileName := fmt.Sprintf("%s.html", s.Title)
-		htmlSavePath := path.Join(domainSubPath, htmlFileName)
-		err = ioutil.WriteFile(htmlSavePath, s.HtmlBody, 0600)
+		htmlSavePath := path.Join(c.Archive, d, htmlFileName)
+		err = ioutil.WriteFile(htmlSavePath, s.HtmlBody, 0777)
 		if err != nil {
 			return err
 		}
 
 		// save the text
 		textFileName := fmt.Sprintf("%s.txt", s.Title)
-		textSavePath := path.Join(domainSubPath, textFileName)
-		err = ioutil.WriteFile(textSavePath, s.TextBody, 0600)
+		textSavePath := path.Join(c.Archive, d, textFileName)
+		err = ioutil.WriteFile(textSavePath, s.TextBody, 0777)
 		if err != nil {
 			return err
 		}
-
-		// save the pdf
-		pdfFileName := fmt.Sprintf("%s.pdf", s.Title)
-		pdfSavePath := path.Join(domainSubPath, pdfFileName)
-		if err := generatePDF(pdfSavePath, s.Url); err != nil {
-			return err
-		}
 	}
-
 	return nil
 }
 
-// AddUrl ...
-func (c *Crawler) AddUrl(url string) error {
-	url = strings.TrimSpace(url)
-	if len(url) == 0 {
-		return errors.New("URL can't be empty or only containing space")
-	}
-
-	if !strings.Contains(url, "://") {
-		url = "http://" + url
-	}
-
-	if !urlRegExp.MatchString(url) {
-		return errors.New("Illegal url:" + url)
-	}
-
+func (c *Crawler) AddUrl(url string) {
 	c.Urls = append(c.Urls, url)
-
-	return nil
 }
 
-// create filename using site title
-// remove illegal characters in the filename
-func createSiteFilename(url string, htmlBody []byte) (string, error) {
-	forbiddenCharactersUnix := [...]rune{'/'}
-	forbiddenCharactersWindows := [...]rune{'/', '<', '>', ':', '"', '\\', '|', '?', '*'}
-	reservedFilenamesWindows := [...]string{"CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9"}
-
-	title, err := getHtmlTitle(htmlBody)
-
-	// if there is no title, do old way of creating hash
-	if err == errNoTitleInHtml {
-		h := sha256.New()
-		_, err = io.WriteString(h, url)
-		if err != nil {
-			return "", err
-		}
-		title = fmt.Sprintf("%x", h.Sum(nil))
-	} else if err != nil {
-		return "", err
-	}
-
-	// Fix if filename is invalid
-	if runtime.GOOS == "windows" { // is windows
-		for _, ch := range forbiddenCharactersWindows {
-			title = strings.ReplaceAll(title, string(ch), "_")
-		}
-		for _, name := range reservedFilenamesWindows {
-			if title == name { // wrap title with quotes
-				title = "'" + title + "'"
-			}
-		}
-	} else { // is unix
-		for _, ch := range forbiddenCharactersUnix {
-			title = strings.ReplaceAll(title, string(ch), "_")
-		}
-	}
-	return title, nil
-}
-
-// Crawl ...
 func (c *Crawler) Crawl() error {
 	for _, u := range c.Urls {
 		fmt.Printf("Crawling %s...\n", u)
 
-		var site Site
+		var site Site  //warning: exported method Crawler.AddUrl should have comment or be unexported (golint)
 
 		htmlBody, err := getHtmlBody(u)
 		if err != nil {
-			return err
+			return err //warning: exported method Crawler.Crawl should have comment or be unexported (golint)
 		}
-
-		title, err := createSiteFilename(u, htmlBody)
-		if err != nil {
-			return err
-		}
-
-		site.Title = title
+		site.HtmlBody = htmlBody
 
 		textBody, err := getTextBody(htmlBody)
 		if err != nil {
 			return err
 		}
-
 		site.TextBody = textBody
+
 		site.Url = u
 
 		c.Sites = append(c.Sites, site)
 	}
-
 	return nil
 }
 
-func getHtmlTitle(body []byte) (title string, err error) {
-	// HTML DOM Document
-
-	r := bytes.NewReader(body)
-	doc, err := goquery.NewDocumentFromReader(r)
-
-	if err != nil {
-		return "", err
-	}
-	titleTag := doc.Find("title").First()
-
-	if titleTag.Size() == 0 {
-		return "", errNoTitleInHtml
-	}
-
-	return titleTag.Text(), nil
-}
-
 func getHtmlBody(url string) (body []byte, err error) {
-	// #nosec - gosec will detect this as a G107 error
-	// the point of this function *is* to accept a variable URL
 	resp, err := http.Get(url)
 	if err != nil {
 		return body, err
 	}
-
 	defer resp.Body.Close()
 
 	htmlBody, err := ioutil.ReadAll(resp.Body)
@@ -251,27 +125,8 @@ func getTextBody(htmlBody []byte) (body []byte, err error) {
 }
 
 func ensureArchive(p string) {
-	err := os.MkdirAll(p, 0700)
+	err := os.MkdirAll(p, 0777)
 	if err != nil {
 		panic(err)
 	}
-}
-
-func generatePDF(path, url string) error {
-	pdfg, err := wkhtmltopdf.NewPDFGenerator()
-	if err != nil {
-		return err
-	}
-
-	pdfg.AddPage(wkhtmltopdf.NewPage(url))
-
-	if err = pdfg.Create(); err != nil {
-		return err
-	}
-
-	if err = pdfg.WriteFile(path); err != nil {
-		return err
-	}
-
-	return nil
 }
